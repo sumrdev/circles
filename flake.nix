6251024c7105fef2390d0890adb24a4c0039d409
TheColorman <github@colorman.me>
2025-02-21 13:18:31 +0100

    Fix devshell

--- flake.nix
+++ flake.nix
@@ -3,9 +3,12 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=release-24.11";
   };
 
-  outputs = { nixpkgs, ... }: let
+  outputs = { self, nixpkgs }: let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in {
     packages.x86_64-linux.default = pkgs.callPackage ./nix/bapctools.nix {};
+    devShells.x86_64-linux.default = pkgs.mkShellNoCC {
+      packages = [ self.outputs.packages.x86_64-linux.default ];
+    };
   };
 }
