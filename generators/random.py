46dec173978cd0fa364a5b750844685148a5a14e
Jogge <61437971+0xjogge@users.noreply.github.com>
2025-02-21 13:54:15 +0100

    Add seeding to random input

--- generators/random.py
+++ generators/random.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 import random
+import sys
+
+random.seed(sys.argv[1])
 
 x1,y1,r1 = [random.randint(-1000,1000), random.randint(-1000,1000), random.randint(0,1000)]
 x2,y2,r2 = [random.randint(-1000,1000), random.randint(-1000,1000), random.randint(0,1000)]
