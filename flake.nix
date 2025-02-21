3f3e081c19f732f10c941b98ac80a9e848058d40
TheColorman <github@colorman.me>
2025-02-21 14:25:16 +0100

    Add new deps

--- flake.nix
+++ flake.nix
@@ -1,10 +1,13 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=release-24.11";
+    nixpkgs-problemtools.url = "github:thecolorman/nixpkgs?ref=add-problemtools";
   };
 
-  outputs = { self, nixpkgs }: let
+  outputs = { self, nixpkgs, nixpkgs-problemtools }: let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+    problemtools = nixpkgs-problemtools.legacyPackages.x86_64-linux.problemtools;
   in {
     packages.x86_64-linux.default = pkgs.callPackage ./nix/bapctools.nix {};
     devShells.x86_64-linux.default = pkgs.mkShellNoCC {
@@ -14,7 +17,11 @@
         gnat
         python3
         ruff
-      ]);
+        texliveMedium
+        pypy3
+      ]) ++ [
+        problemtools
+      ];
     };
   };
 }
