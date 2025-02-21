42d484568eb175515271ee5bf3303eb3e8c6e768
Jogge <61437971+0xjogge@users.noreply.github.com>
2025-02-21 13:52:39 +0100

    Random use new input format now

--- generators/random.py
+++ generators/random.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+
 import random
 
-# Read the second... arguments as ints Example call:
-# example.py {seed} 1 2 3 4
-x1,y1,r1,x2,y2,r2 = [random.randint(-1000,1000), random.randint(-1000,1000), random.randint(0,1000), random.randint(-1000,1000), random.randint(-1000,1000), random.randint(0,1000)]
+x1,y1,r1 = [random.randint(-1000,1000), random.randint(-1000,1000), random.randint(0,1000)]
+x2,y2,r2 = [random.randint(-1000,1000), random.randint(-1000,1000), random.randint(0,1000)]
 
-# Print in standard format, i.e. one line with the number of values,
-# followed by the space-separated values.
-print(x1,y1,r1,x2,y2,r2)
+print(x1, y1, r1)
+print(x2, y2, r2)
