a9582e435dc24eeea6bbe5da6fc10ba38115a2f8
Marius Nielsen <mawn@itu.dk>
2025-02-21 14:28:29 +0100

    no decimals

--- submissions/accepted/marius.py
+++ submissions/accepted/marius.py
@@ -12,13 +12,13 @@ if( d < r1 + r2):
     rr2 = r2 * r2
 
     if d<=abs(r2-r1):
-        print(math.pi * min(rr1,rr2))
+        print(int(math.pi * min(rr1,rr2)))
         exit(0)
 
     t1 = 2 * math.acos((rr1 + d * d - rr2) / (2 * d * r1))
     t2 = 2 * math.acos((rr2 + d * d - rr1) / (2 * d * r2))
 
-    print(0.5 * (rr1 *  (t1 - math.sin(t1))) + rr2 * (t2 - math.sin(t2)))
+    print(int(0.5 * (rr1 *  (t1 - math.sin(t1))) + rr2 * (t2 - math.sin(t2))))
     exit(0)
 
 print(0)
--- submissions/wrong_answer/almost-right.py
+++ submissions/wrong_answer/almost-right.py
@@ -2,23 +2,27 @@
 
 import math
 
-x1, y1, r1 = map(int, input().split())
-x2, y2, r2 = map(int, input().split())
+try:
+    x1, y1, r1 = map(int, input().split())
+    x2, y2, r2 = map(int, input().split())
 
-d = math.hypot(x2-x1,y2-y1)
+    d = math.hypot(x2-x1,y2-y1)
 
-if( d < r1 + r2):
-    rr1 = r1 * r1
-    rr2 = r2 * r2
+    if( d < r1 + r2):
+        rr1 = r1 * r1
+        rr2 = r2 * r2
 
-    if d<=abs(r2-r1):
-        print(math.pi * min(rr1,rr2))
+        if d<=abs(r2-r1):
+            print(math.pi * min(rr1,rr2))
+            exit(0)
+
+        t1 = 2 * math.acos(rr1 + d * d - rr2) / (2 * d * r1)
+        t2 = 2 * math.acos(rr2 + d * d - rr1) / (2 * d * r2)
+
+        print(0.5 * (rr1 *  (t1 - math.sin(t1))) + rr2 * (t2 - math.sin(t2)))
         exit(0)
 
-    t1 = 2 * math.acos(rr1 + d * d - rr2) / (2 * d * r1)
-    t2 = 2 * math.acos(rr2 + d * d - rr1) / (2 * d * r2)
+    print(0)
 
-    print(0.5 * (rr1 *  (t1 - math.sin(t1))) + rr2 * (t2 - math.sin(t2)))
-    exit(0)
-
-print(0)
+except ValueError:
+    print(0)
