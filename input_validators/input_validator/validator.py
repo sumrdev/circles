663d2cf2b39c42c804cd69f7194f2966acaa0dbc
Jogge <61437971+0xjogge@users.noreply.github.com>
2025-02-21 13:43:52 +0100

    Refacor input validator

--- input_validators/input_validator/validator.py
+++ input_validators/input_validator/validator.py
@@ -1,16 +1,41 @@
 import sys
-def check_num(a, is_coord):
+
+def validate_number(n):
     try:
-        int(a)
-        if int(a)!=str(int(a)): exit(43)
-        if is_coord and a < -1000: exit()
-        if a > 1000: exit(43)
-        if a < 0: exit(43)
+        # Check if valid number
+        int(n)
+
+        # Check for leading zeros
+        if int(n)!=str(int(n)):
+            exit(43)
     except ValueError:
         exit(43)
 
-x1,y1,r1,x2,y2,r2 = map(int, input().split())
+def check_radius(r):
+    validate_number(r)
+
+    if r < 0 or r > 1000: 
+            exit(43)
+
+def check_cord(c):
+    validate_number(c)
+
+    if c < -1000 or c > 1000: 
+            exit(43)
+
+x1, y1, r1 = map(int, input().split())
+x2, y2, r2 = map(int, input().split())
+
+# Check firste circle
+check_cord(x1)
+check_cord(y1)
+check_radius(r1)
+
+# Check second circle
+check_cord(x2)
+check_cord(y2)
+check_radius(r2)
 
 if sys.stdin.readline() != "": exit(43)
 
-
+exit(42)
